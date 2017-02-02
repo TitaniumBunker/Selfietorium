7dfb61bb1ad6ee934fe42e65734aef0b91e3f80a
Mike Hingley <mike@titaniumbunker.com>
2017-02-02 22:35:02 +0000

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../selfietorium'))
+sys.path.insert(0, os.path.abspath('../../selfietorium'))
 
 
 # -- General configuration ------------------------------------------------
